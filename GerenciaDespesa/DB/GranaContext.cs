ffa3bd94c4419612671424b30f124088aa8471a8
Felipe <feligaquino@gmail.com>
2017-09-28 12:40:17 -0300

    Comecei finalmente

---
+++ GerenciaDespesa/DB/GranaContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using GerenciaDespesa.Model;
+
+namespace GerenciaDespesa.DB
+{
+    class GranaContext : DbContext
+    {
+        public DbSet<Despesa> Despesas { get; set; }
+        public DbSet<Receita> Receitas { get; set; }
+
+        public GranaContext()
+        {
+            DropCreateDatabaseIfModelChanges<GranaContext> init = new DropCreateDatabaseIfModelChanges<GranaContext>();
+            Database.SetInitializer<GranaContext>(init);
+        }
+    }
+}
---
+++ GerenciaDespesa/Model/Despesa.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GerenciaDespesa.Model
+{
+    class Despesa
+    {
+        public long Id { get; set; }
+        public string Nome { get; set; }
+        public double Valor { get; set; }
+        public DateTime Data { get; set; }
+        public string Tipo { get; set; }
+
+        public override string ToString()
+        {
+            return this.Nome + " - " + this.Valor + " - " + this.Tipo + " - " + this.Data.ToString("dd/MM/yyyy");
+        }
+    }
+}
